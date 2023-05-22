f024dfca7390b628b47fa6ad6bb2305ea7526be0
WaqyWise <vakulenko.a@ukr.net>
2023-05-22 17:00:38 +0300

    FakeStore

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import ProductList from './components/ProductList';
-import ProductPage from './components/ProductPage';
-import CartPage from './components/CartPage';
-import Header from './components/HeaderSet/Header';
-import Footer from './components/Footer';
-import NotFoundPage from './components/NotFoundPage';
+import React, { useEffect, useState } from "react";
+import Header from "./components/header/Header";
+import Search from "./components/search/Search";
+import AddProducts from "./components/addproducts/AddProduct";
+import CardBody from "./components/cards/CardBody";
+import Button from "./components/button/Button";
 
+import "./App.css";
 const App = () => {
+  const [items, setItem] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [addedItems, setAddedItem] = useState([]);
+  const [showAddProducts, setShowAddProducts] = useState(false);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/")
+      .then((res) => res.json())
+      .then((data) => setItem(data));
+    console.count("hi");
+  }, []);
+  function changingSrarchData(e) {
+    setSearchValue(e.target.value);
+  }
+  const itmesFilter = items.filter((item) =>
+    item.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  function addItem(item) {
+    item.addNumber = 1;
+    const itemArr = addedItems;
+    setAddedItem([...itemArr, item]);
+  }
+  // console.log(addedItems);
+  function removeItem(item) {
+    const newItems = addedItems.filter((addedItem) => addedItem.id !== item.id);
+    setAddedItem(newItems);
+    // console.log(addedItems);
+  }
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/products/:id" element={<ProductPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route component={<NotFoundPage />} />
-        </Routes>
-        <Footer />
+    <div>
+      {/* <Header /> */}
+
+      <div className="body__container">
+        <div className="nav">
+          <Header />
+          <div className="nav-right">
+            <Search
+              products={items}
+              value={searchValue}
+              onChangeData={changingSrarchData}
+            />
+            <Button num={addedItems.length} click={setShowAddProducts} />
+          </div>
+        </div>
+
+        {showAddProducts && (
+          <AddProducts
+            click={setShowAddProducts}
+            items={addedItems}
+            removeItem={removeItem}
+            setAddedItem={setAddedItem}
+          />
+        )}
+        <CardBody
+          products={itmesFilter}
+          addItem={addItem}
+          removeItem={removeItem}
+          addedItems={addedItems}
+        />
       </div>
-    </Router>
+    </div>
   );
 };
 
 export default App;
-
